b8b199184ff81a960b45a8db32ec80d9c6789877
Artur Gomes <artur.o.gomes@gmail.com>
2025-11-19 09:22:52 -0300

    feat: add core library exports and type definitions for Game of Life frontend

---
+++ packages/web/src/lib/index.ts
@@ -0,0 +1,36 @@
+/**
+ * Core library exports for Game of Life frontend
+ */
+
+// API Client
+export {
+  buildWebSocketUrl,
+  createBoard,
+  getNextGeneration,
+  getStateAtGeneration,
+  startFinalStateCalculation,
+} from './api-client';
+// Board Utilities
+export {
+  boardsEqual,
+  cloneBoard,
+  countAliveCells,
+  createEmptyBoard,
+  createRandomBoard,
+  getBoardDimensions,
+  resizeBoard,
+  validateBoard,
+} from './board-utils';
+// Patterns
+export {
+  ALL_PATTERNS,
+  getAllPatternNames,
+  getPatternByName,
+  METHUSELAHS,
+  OSCILLATORS,
+  SPACESHIPS,
+  STILL_LIFES,
+} from './patterns';
+export type { WebSocketCallbacks } from './websocket-client';
+// WebSocket Client
+export { GameOfLifeWebSocket } from './websocket-client';
